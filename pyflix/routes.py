4287e19a87cccb1d931cae19c28c696eaf89233d
antoniouaa <antoniouaa@hotmail.com>
2021-03-19 21:50:54 +0000

    style fixes and global object to track the current page

--- pyflix/routes.py
+++ pyflix/routes.py
@@ -5,7 +5,8 @@ blueprint = Blueprint("api_blueprint", __name__)
 
 keys = {
     "space": "space",
-    "skip_forward": "shift+n",
+    "move_next": "shift+n",
+    "move_prev": "shift+p",
     "volume_up": "up, up, up",
     "volume_down": "down, down, down",
     "toggle_full_screen": "f",
@@ -13,12 +14,21 @@ keys = {
     "skip_10_forward": "right",
     "mute": "m",
     "skip_intro": "s",
+    "speed_playback": "shift+>",
+    "slow_playback": "shift+<",
 }
 
+current_page = ""
 
-@blueprint.route("/", methods=["GET"])
-def index():
-    return render_template("index.html", title="Index")
+
+@blueprint.route("/pages", methods=["POST"])
+def page_post():
+    global current_page
+    if "page" in request.form:
+        current_page = request.form["page"]
+    return render_template(
+        "commands.html", title=current_page.title(), page=current_page
+    )
 
 
 @blueprint.route("/click", methods=["POST"])
@@ -26,12 +36,19 @@ def click():
     try:
         action = next(iter(request.form))
         if action in keys:
-            print(f"Performing action: {keys[action]}")
+            print(f"Performing action: {action}")
         keyboard.send(keys[action])
     except StopIteration:
         print("Form empty")
         action = "error"
     finally:
-        response = make_response(redirect("/api"))
+        response = make_response(redirect("/api/pages", code=307))
         response.headers["Option-Clicked"] = action
         return response
+
+
+@blueprint.route("/", methods=["GET"])
+def index():
+    global current_page
+    current_page = ""
+    return render_template("index.html", title="pyflix")
