577e54f5b4e48e17b0e8342825f3076c754cb2ea
antoniouaa <antoniouaa@hotmail.com>
2021-06-14 14:05:38 +0100

    added a run task to pyproject because i cant remember how to launch this to save my life

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from pyflix import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=5000, load_dotenv=True)
+    app.run(host="192.168.0.58", port=5000, load_dotenv=True)
